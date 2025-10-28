b4f92fbe70c9adaeb72ee05c912c01004142f890
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-10-28 13:34:40 +0300

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
