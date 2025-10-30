c3c8dfbc9ca41c003282090306aafc4d366b2d4d
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-10-30 13:21:31 +0300

    added name

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -21,28 +21,9 @@ function App() {
       </div>
        <div>
       <h1>User Profile</h1>
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile "name=", "Alice", "age=", "25", "bio=", "Loves hiking and photography" />
     </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
