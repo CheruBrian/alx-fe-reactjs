fb767ce1917409c2690d9d7ed0dc34f9657598d8
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-05 11:59:13 +0300

    added new dir

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+Function Footer() {
+    return (
+        <footer style={{background: 'gray', margin: '0', padding: '0' }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+Function MainContent() {
+    return (
+        <main style={{backgroundColor: 'gray', color: 'black', fontSize: '13px' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
