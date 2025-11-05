11a91f52aa5c4f5d3791a18aa0692dd60576de7d
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-05 13:08:11 +0300

    moved

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,31 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './components/Header.jsx'
-import MainContent from './components'
-import Footer from './components/Footer.jsx'
-import UserProfile from './components/UserProfile'
+import ProfilePage from './ProfilePage';
+import UserContext from './components/UserContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-      <div>
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <Footer />
-      </div>
-       <div>
-      <h1>User Profile</h1>
-      <UserProfile "name=", "Alice", "age=", "25", "bio=", "Loves hiking and photography" />
-    </div>
-      
-    </>
-  )
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
