bc6963bd30a7a4f86f705ba8f0a92e46ebfc6ea7
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-10 16:50:22 +0300

    update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,35 @@
-import create from 'zustand'
+import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
-}));
-
+export const useRecipeStore = create(
+  persist(
+    (set, get) => ({
+      recipes: [],
+      
+      // Add a new recipe
+      addRecipe: (recipe) => set((state) => ({
+        recipes: [...state.recipes, { ...recipe, id: Date.now().toString() }]
+      })),
+      
+      // Update an existing recipe
+      updateRecipe: (id, updatedRecipe) => set((state) => ({
+        recipes: state.recipes.map(recipe =>
+          recipe.id === id ? { ...updatedRecipe, id } : recipe
+        )
+      })),
+      
+      // Delete a recipe
+      deleteRecipe: (id) => set((state) => ({
+        recipes: state.recipes.filter(recipe => recipe.id !== id)
+      })),
+      
+      // Get a specific recipe by ID
+      getRecipe: (id) => {
+        return get().recipes.find(recipe => recipe.id === id);
+      }
+    }),
+    {
+      name: 'recipe-storage',
+    }
+  )
+);
