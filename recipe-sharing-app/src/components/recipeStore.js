7b488c59f2bddf8c960f972ee5a39947175eec20
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-10 16:48:50 +0300

    update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,7 +1,9 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware';
 
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
+
