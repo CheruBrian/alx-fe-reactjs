a3d7551bdd6b8e8fd8354117fa4f2e5541b2faf1
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-10 17:50:27 +0300

    update

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
  import { useRecipeStore } from './recipeStore';
+ import SearchBar from'./SearchBar';
 
 
   const RecipeList = () => {
@@ -14,6 +15,20 @@
         ))}
       </div>
     );
+     <div>
+      <h2>All Recipes</h2>
+      <SearchBar />
+      <button onClick={handleAddExample}>Add Example Recipe</button>
+
+      <ul style={{ marginTop: 16 }}>
+        {visibleRecipes.length > 0 ? (
+          visibleRecipes.map((r) => (
+            <li key={r.id} style={{ marginBottom: 8 }}>
+              <Link to={`/recipes/${r.id}`}>{r.title}</Link> — {r.time} mins
+            </li>
+            </ul>
+          </div>
+        ))
   };
 
   export default RecipeList;
