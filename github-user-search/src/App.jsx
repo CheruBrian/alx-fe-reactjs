d9c9c5028098b12e2aed8c7c0e5cbca65b0c7d86
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-18 08:55:20 +0300

    update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import serach from './components/SearchBar'
+import SearchBar from './components/SearchBar'
+import UserCard from './components/'UserCard'
+import { getGitHubUser } './services/githubService'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleSearch = async (username) => {
+    setError("");
+    setUser(null);
+
+    try {
+      const data = await getGithubUser(username);
+      setUser(data);
+    } catch (err) {
+      setError("User not found");
+    }
+  };
+   return (
+    <div className="app-container">
+      <h1>GitHub User Search</h1>
+      <SearchBar onSearch={handleSearch} />
+
+      {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <UserCard user={user} />
+    </div>
+  );
 }
 
 export default App
