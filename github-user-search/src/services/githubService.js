85a74eb846b49ec0ced3a2747872b53794a10849
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-21 12:36:46 +0300

    fixing bugs

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,14 +1,23 @@
 import axios from "axios";
 
-const GITHUB_URL = "https://api.github.com/users/";
+const SEARCH_URL = "https://api.github.com/search/users?q";
 
-export const fetchUserData = async (username) => {
+export const advancedSearchUsers = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>${minRepos}`;
+
+  const trimmed = query.trim().replace(/ /g, "+");
+
   try {
-    const response = await axios.get(GITHUB_URL + username, {
+    const response = await axios.get(SEARCH_URL + trimmed, {
       headers: {
         Authorization: `Bearer ${import.meta.env.VITE_APP_GITHUB_API_KEY || ""}`,
       },
     });
+
     return response.data;
   } catch (error) {
     throw error.response?.data || error;
