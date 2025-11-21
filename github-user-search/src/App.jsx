8f6d01939a1dbbc70616634fb88d6c35f2cbd30f
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-11-21 13:00:25 +0300

    updated

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import SearchBar from './components/SearchBar'
-import UserCard from './components/'UserCard'
 import { getGitHubUser } './services/githubService'
 
 function App() {
