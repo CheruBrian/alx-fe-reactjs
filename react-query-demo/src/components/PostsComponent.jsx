a99fbd6e4ad9ee7a35bc780ee351502fac8ed218
Brian Cheru <cheruiyotbrian2000@gmail.com>
2025-12-08 15:19:30 +0300

    Data fetching component created

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+
+export default function PostsComponent() {
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      return res.json();
+    }
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching posts</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={() => refetch()}>Refetch</button>
+
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
